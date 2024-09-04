f9a4674e740bb8a55044453feb04de9997834389
LeandroBarrera <leann.barrera@gmail.com>
2024-09-04 13:28:21 -0300

    Inventario cargar modal. Sweet Alert

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/index')]
+#[Route('/')]
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
--- src/Controller/InventarioController.php
+++ src/Controller/InventarioController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Producto;
+use App\Form\ProductoCargaType;
+use App\Repository\ProductoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,9 +15,22 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/inventario')]
 class InventarioController extends AbstractController
 {
-    #[Route('/', name: 'app_inventario')]
-    public function index(): Response
+    #[Route('/', name: 'app_inventario_index', methods: ['GET'])]
+    public function index(ProductoRepository $productoRepository): Response
     {
-        return $this->render('logistica\inventario\index.html.twig');
+        return $this->render('logistica/inventario/index.html.twig', [
+            'productos' => $productoRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/carga', name: 'app_inventario_carga', methods: ['GET', 'POST'])]
+    public function carga(Request $request, ProductoRepository $productoRepository, EntityManagerInterface $entityManager): Response
+    {
+        $productoId = $request->get('productoId');
+        $stock_total= $request->get('stock_total');   
+        $producto = $productoRepository->find($productoId);
+
+        $producto->setStock($stock_total);        
+        $entityManager->flush();
     }
 }
